420feae25bec565b98921258cf33e9b3e65d5e4a
Mike B <115735557+mbartnett@users.noreply.github.com>
2022-11-04 11:29:00 -0500

    ready to start with the JavaScript

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 // Assignment Code
+
+// IF a user clicks the generate button 
+// THEN #password-card.display:none;
+// THEN #character-card.display:inline;
+
 var generateBtn = document.querySelector("#generate");
 
+function cardBody() {
+  document.getElementById('password-card');
+  document.getElementById('character-card');
+}
 
+
+
+
+
+/*
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -10,46 +24,7 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-/*
-GIVEN I need a new, secure password
-WHEN I click the button to generate a password
-THEN I am presented with a series of prompts for password criteria
-WHEN prompted for password criteria
-THEN I select which criteria to include in the password
-WHEN prompted for the length of the password
-THEN I choose a length of at least 8 characters and no more than 128 characters
-WHEN asked for character types to include in the password
-THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-WHEN I answer each prompt
-THEN my input should be validated and at least one character type should be selected
-WHEN all prompts are answered
-THEN a password is generated that matches the selected criteria
-WHEN the password is generated
-THEN the password is either displayed in an alert or written to the page
 */
 
-/* CODE FROM THE WEB */
-
-/*var password=document.getElementById("password");
-
- function genPassword() {
-    var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var passwordLength = 12;
-    var password = "";
- for (var i = 0; i <= passwordLength; i++) {
-   var randomNumber = Math.floor(Math.random() * chars.length);
-   password += chars.substring(randomNumber, randomNumber +1);
-  }
-        document.getElementById("password").value = password;
- }
-
-function copyPassword() {
-  var copyText = document.getElementById("password");
-  copyText.select();
-  document.execCommand("copy");  
-}
-*/
