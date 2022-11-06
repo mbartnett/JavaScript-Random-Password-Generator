3957141fc63744415abc5b65794af571c5736b1d
Mike B <115735557+mbartnett@users.noreply.github.com>
2022-11-06 14:05:51 -0600

    adding changes made during tutoring session

--- script.js
+++ script.js
@@ -3,22 +3,32 @@
 //generateBtn.onclick = function(){
 var generateBtn = document.querySelector("#generate");
 
-var charCount;
-var uppercase;
-var lowercase;
-var number;
-var specialChar;
+let big = ["A","B",];
+let little = ["A","B",];
+let num = ["A","B",];
+let special = ["A","B",];
+
+let possibleChars = [];
 
 function generatePassword (){
-  charCount = window.prompt("How many characters would you like your password to be?");
-  console.log(charCount);
-  IF THEN
-}
+  let charCount = window.prompt("How many characters would you like your password to be?");
+  if (parseInt)(charCount)< 8 || (parseInt)(charCount)>128
+  alert ("Your password must be between 8 and 128 characters.");{
+  }
+  let uppercase = window.confirm("Would you like to use uppercase letters?");
+  let lowercase = window.confirm("Would you like to use lowercase letters?");
+  let number = window.confirm("Would you like to use numbers?");
+  let specialChar = window.confirm("Would you like to use special characters?");
 
-uppercase = window.confirm("Would you like to use uppercase letters?");
-lowercase = window.confirm("Would you like to use lowercase letters?");
-number = window.confirm("Would you like to use numbers?");
-specialChar = window.confirm("Would you like to use special characters?");
+if(uppercase == false && lowercase == false && number == false && specialChar == false){
+    alert ("You need to select at least one character type.");
+  }
+
+// if (lowercase =="true"){
+// possibleChars = possibleChars.push(letters);
+// }
+
+}
 
 // Write password to the #password input
 function writePassword() {
